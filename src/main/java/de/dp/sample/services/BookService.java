5d5223f16822e36513afa240e31da9ed06314b94
rgmonk <rg-github@online.de>
2020-10-07 07:59:19 +0000

    java util

--- src/main/java/de/dp/sample/services/BookService.java
+++ src/main/java/de/dp/sample/services/BookService.java
@@ -1,5 +1,7 @@
 package de.dp.sample.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import de.dp.sample.models.Book;
